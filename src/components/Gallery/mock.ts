02f8dd17c172ecb5e0ffda0e3c94b85c09719cb3
Rodrigo_JS <rodrigojeansantos@gmail.com>
2021-10-24 18:52:30 -0300

    feat: Add componentes Gallery and TexContent

---
+++ src/components/Gallery/mock.ts
@@ -0,0 +1,26 @@
+export default [
+    {
+      src: 'https://www.gironews.com/imagens/225x335/eaf1e5ff-340b-446a-b1b7-627f61a69cd9_Nota1607215.jpg',
+      label: 'Gallery Image 1'
+    },
+    {
+      src: 'https://files.sunoresearch.com.br/n/uploads/2021/04/86fab5f8-duratex-800x450.png',
+      label: 'Gallery Image 2'
+    },
+    {
+      src: 'https://s2.glbimg.com/i_0Trjq8yDMtNo94rGakWtOLfkc=/0x0:1055x573/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2021/l/C/mvF1MwQg2pK2oaJYasRA/104.jpg',
+      label: 'Gallery Image 3'
+    },
+    {
+      src: 'https://dexco.s3.sa-east-1.amazonaws.com/images/areas-de-negocio.jpg',
+      label: 'Gallery Image 4'
+    },
+    {
+      src: 'https://nide.com.br/wp-content/uploads/2021/08/1626550380117.jpg',
+      label: 'Gallery Image 5'
+    },
+    {
+      src: 'https://files.sunoresearch.com.br/n/uploads/2019/10/0c8a840b-duratex-800x450.jpg',
+      label: 'Gallery Image 6'
+    }
+  ]
